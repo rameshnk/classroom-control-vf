0742466acf40dff67fec8bac4b13b787ab49925f
rameshnk <rameshnagaraj@hotmail.com>
2016-03-23 09:29:38 +0530

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,41 @@
+class nginx{
+  #package nginx
+  package {'nginx':
+    ensure => present,
+  }
+  
+  # document root/var/www
+  file{'/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root'
+    mode => '0755'
+  }
+  
+  #index.html
+  file{'/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root'
+    mode => '0755'
+    source => 'puppet:///modules/nginx/index.html'
+  }
+  
+  #config file nginx.conf
+  file{'/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root'
+    mode => '0755'
+    source => 'puppet:///modules/nginx/nginx.conf'
+    require => Package['nginx']
+    notify => Service['nginx']
+  }
+  
+  file{'/etc/nginx/conf.d':
+    ensure => file,
+    owner => 'root',
+    group => 'root'
+    mode => '0755'
+  }
+}
